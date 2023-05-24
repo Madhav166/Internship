d4aad26b3a7a0aaeeea683de18c8e4903f1f396a
Madhav166 <12002080601057@adit.ac.in>
2023-05-24 11:53:24 +0530

    Day 5

--- my-app/src/Components/Login.jsx
+++ my-app/src/Components/Login.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Avatar, Button, Popover, TextField } from '@mui/material' 
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import './component.css';
-
+import {Formik} from "formik";
+import * as Yup from "yup";
 
 export default function Login(){
-    const [name , setName] = useState("Madhav");
-    const [email , setEmail] = useState("madhav@gmail.com");
+    //const [Uname , setName] = useState("Madhav");
+    //const [email , setEmail] = useState("madhav@gmail.com");
     const [open , setOpen] = useState(false);
     const [anchorEl, setAnchorEl] = useState(null);
     const Navigate = useNavigate();
-    const whenButtonIsClick = () => {
-        console.log("Welcome",email,name);
+
+    useEffect(() => {
+
+    } ,[]);
+
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().min(3,"Please Enter Atleast 3 Character.").required("Enter your name"),
+        email: Yup.string().email("Please Enter valid email Address.").required("Enter your email"),
+    });
+    const initialValues = {
+        name: "",
+        email: "",
+    };
+    const onFormSubmit = (values) => {
+        console.log("on the form submitted", values);
+        alert("Form submitted");
         Navigate("/home");
-        alert("Welcome " + name);
-    }
+        
+    };
+
     const handleClick = (event) => {
         setAnchorEl(event.currentTarget);
         setOpen(true);
@@ -33,10 +49,27 @@ export default function Login(){
                     <Avatar sx={{ bgcolor: 'blue'}}>MD</Avatar>
                 </div>
             </div>
-            <div className='login'>
-                <TextField variant="outlined" label = "Name" type="text" value={name} placeholder='name' onChange={(e) => setName(e.target.value)}/> <br />
-                <TextField variant="outlined" label = "Email"  type="email" value={email} placeholder='email' onChange={(e) => setEmail(e.target.value)}/><br />
-                <Button variant="contained" onClick={whenButtonIsClick}>Login</Button>
+            <div>
+                <Formik initialValues = {initialValues}
+                    validationSchema = {validationSchema}
+                    onSubmit = {onFormSubmit}>
+                {({value ,errors, touched,isSubmitting ,handleChange,handleBlur ,handleSubmit}) =>(
+                    <form onSubmit={handleSubmit}>
+                    <div className='login'>
+                    <TextField variant="outlined" label = "Name" type="text" id='name' name = "name" placeholder='name' onChange={handleChange} onBlur={handleBlur}/> 
+                    {touched.name && errors.name && <span className='printError'> {errors.name}</span>}
+                    <br />
+                    
+                    
+                    <TextField variant="outlined" label = "Email"  type="email" id='email'  name = "email"  placeholder='email' onChange={handleChange} onBlur={handleBlur}/>
+                    {touched.email && errors.email && <span className='printError'> {errors.email}</span>}
+                    <br />
+                    <Button variant="contained" type='submit'>Login</Button>
+                    </div>
+                    </form>
+                )}
+                
+                </Formik>
             </div>
             
             <Popover
